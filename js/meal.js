dca8d653a474b8d468c5013d56fd9deb52908f0c
masomdewan24 <masom24@gmail.com>
2022-08-29 00:22:18 +0600

    last

--- js/meal.js
+++ js/meal.js
@@ -1,5 +1,6 @@
-const lodeMeals = () => {
-    fetch('https://www.themealdb.com/api/json/v1/1/search.php?f=a')
+const lodeMeals = (search) => {
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`
+    fetch(url)
     .then(res => res.json())
     .then(data => displyMeals(data.meals))
 }
@@ -7,15 +8,17 @@ const lodeMeals = () => {
 const displyMeals = meals =>{
     
     const mealsContainer = document.getElementById('meal-container')
+    mealsContainer.innerHTML = '';
     meals.forEach(meal =>{
+        console.log(meal);
         const mealDiv = document.createElement('div');
         mealDiv.classList.add('col');
         mealDiv.innerHTML = `
-        <div class="card">
-                        <img src="..." class="card-img-top" alt="...">
+        <div onclick="loadeMealDetails(${meal.idMeal})" class="card">
+                        <img src="${ meal.strMealThumb}" class="card-img-top" alt="...">
                         <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <h5 class="card-title"> ${meal.strMeal}</h5>
+                        <p class="card-text">${meal.strInstructions.slice(0, 180)}</p>
                         </div>
                   </div>
         
@@ -25,4 +28,36 @@ const displyMeals = meals =>{
         
 }
 
-lodeMeals();
+const searchFood = () =>{
+    const searchFeald = document.getElementById('search-field');
+    const searchText = searchFeald.value;
+    lodeMeals(searchText);
+    searchFeald.value="";
+
+}
+
+const loadeMealDetails = (idMeal) =>{
+const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`;
+
+fetch(url)
+.then(res => res.json())
+.then(data => displyMealsDetales(data.meals[0]))
+
+}
+const displyMealsDetales = meal =>{
+const detailContainer = document.getElementById('detail-container');
+detailContainer.innerHTML = "";
+const mealDiv = document.createElement('div');
+mealDiv.classList.add('card');
+mealDiv.innerHTML =`
+<img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+    <div class="card-body">
+    <h5 class="card-title"> ${meal.strMeal}</h5>
+    <p class="card-text">${meal.strInstructions.slice(0, 180)}</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+`;
+detailContainer.appendChild(mealDiv);
+}
+
+lodeMeals('');
